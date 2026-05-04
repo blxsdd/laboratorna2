bd3921f9e474c6ef4c366adcade90c6d42c863c9
blxsdd <polskiyroman511@gmail.com>
2026-05-04 20:11:50 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
-// завдання 2
-let object = {};
-function isEmpty(obj) {
-    for (let key in obj) {
-        return false;
-    }
-    return true;
-}
-alert(isEmpty(object));
-object["80 кілограмів"] = "великий";
-alert(isEmpty(object))
+// завдання 1
+let user = {}
+user.name = "Марк"
+user.surname = "Сміт"
+user.name = "Тарас"
+delete user.name
+
+console.log(user)
+
+
